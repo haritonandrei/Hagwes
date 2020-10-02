cbad6f9926772e8be40d30d1d48afc411fd9dcc2
Hariton Andrei Marius <andreimariushariton@gmail.com>
2020-10-02 16:55:52 +0200

    Added description meta to index

--- interface/templates/hws/web/index.php
+++ interface/templates/hws/web/index.php
@@ -15,6 +15,7 @@
 
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+	<meta name="description" content="<?php echo($website_description); ?>">
 	
 	<title>
 	<?php
