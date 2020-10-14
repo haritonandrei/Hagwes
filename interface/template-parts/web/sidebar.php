fd729ff408d3fb3777a67aa5284200a6c85e8b92
Hariton Andrei Marius <andreimariushariton@gmail.com>
2020-10-14 12:03:51 +0200

    Themes Fix

--- interface/template-parts/web/sidebar.php
+++ interface/template-parts/web/sidebar.php
@@ -1,18 +1,18 @@
 <h3>Pages</h3>
 <ul class="list">
-<li><a href="page.php?id=1" class="link">About</a></li>
-<li><a href="page.php?id=2" class="link">Categories</a></li>
+<li class="list-item"><a href="page.php?id=1" class="link">About</a></li>
+<li class="list-item"><a href="page.php?id=2" class="link">Categories</a></li>
 </ul>
 
 <h3>Last Articles</h3>
 <ul class="list">
-<li><a href="article.php?id=3" class="link">Article 3</a></li>
-<li><a href="article.php?id=2" class="link">Article 2</a></li>
-<li><a href="article.php?id=1" class="link">Article 1</a></li>
+<li class="list-item"><a href="article.php?id=3" class="link">Article 3</a></li>
+<li class="list-item"><a href="article.php?id=2" class="link">Article 2</a></li>
+<li class="list-item"><a href="article.php?id=1" class="link">Article 1</a></li>
 </ul>
 
 <h3>Links</h3>
 <ul class="list">
-<li><a href="search.php" class="link">Search</a></li>
-<li><a href="../administration/index.php" class="link">Login</a></li>
+<li class="list-item"><a href="search.php" class="link">Search</a></li>
+<li class="list-item"><a href="../administration/index.php" class="link">Login</a></li>
 </ul>
