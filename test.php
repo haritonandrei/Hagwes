46824bf131c6a84a77edd20088e93edd1d5cfb56
Hariton Andrei Marius <andreimariushariton@gmail.com>
2020-10-17 17:58:38 +0200

    Clean files

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$datetime = "2020-10-13 09:47:19";
-$date = explode(" ", $datetime)[0];
-
-list($year, $month, $day) = explode("-", $date);
-echo "Month: $month; Day: $day; Year: $year<br />\n";
-
-?>
