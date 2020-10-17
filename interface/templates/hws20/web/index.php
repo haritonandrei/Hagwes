904ec60abbd85acd7f76cb3f9a01607d78c29e03
Hariton Andrei Marius <andreimariushariton@gmail.com>
2020-10-17 18:10:04 +0200

    Themes Update

--- interface/templates/hws20/web/index.php
+++ interface/templates/hws20/web/index.php
@@ -115,7 +115,7 @@
 									<span><img class=\"icon\" src=\"../interface/templates/$template_web/images/icons/sun.png\" title=\"Date\" /> ".date("l jS F Y", strtotime($article -> getDate()))."</span>
 								</div>
 								<p class=\"description\">".$article->getDescription()."</p>
-								<p class=\"chip\"><a href=\"article.php?id=".$article->getIdentifier()."\">Read more &#10141;</a></p>
+								<p class=\"chip\"><a class=\"link-icon\" href=\"article.php?id=".$article->getIdentifier()."\">Read more <img class=\"link-icon__button\" src=\"../interface/templates/$template_web/images/icons/arrow2_right.png\" title=\"Read more\" /></a></p>
 								<hr>"
 							);
 						}
